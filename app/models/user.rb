75c5e45d014ffc876627cb1ede4ac5158541a636
MatsunagaNaoya <60386488+MatsunagaNaoya@users.noreply.github.com>
2022-10-12 19:55:09 +0900

    userモデル追加

---
+++ app/models/user.rb
@@ -0,0 +1,18 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+#  has_many :room_users
+#  has_many :rooms, through: :room_users
+#  has_many :messages
+       
+  validates :nickname, presence: true
+  validates :last_name, presence: true
+  validates :first_name, presence: true
+  validates :last_name_kana, presence: true
+  validates :first_name_kana, presence: true
+  validates :birth_date, presence: true
+
+end
