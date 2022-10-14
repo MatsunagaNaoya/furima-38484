29303b881ee96182779f385c9634c829555038bf
MatsunagaNaoya <60386488+MatsunagaNaoya@users.noreply.github.com>
2022-10-14 14:12:59 +0900

    任意修正箇所・動作確認済み

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up,
-                                      keys: [:nickname, :email, :password, :password_confirmation, :last_name, :first_name, :first_name_kana, :last_name_kana,
+                                      keys: [:nickname, :last_name, :first_name, :first_name_kana, :last_name_kana,
                                              :birth_date])
   end
 end
