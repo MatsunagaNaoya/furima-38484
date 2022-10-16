6bd5b14c1a376c34e8e6612ca9ac5259ad867219
MatsunagaNaoya <60386488+MatsunagaNaoya@users.noreply.github.com>
2022-10-16 15:39:26 +0900

    改行

--- app/models/item.rb
+++ app/models/item.rb
@@ -11,7 +11,12 @@ class Item < ApplicationRecord
   with_options presence: true do
     validates :image, :name, :info
     validates :price, format: { with: /\A[0-9]+\z/ },
-              numericality:{ only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999}
+              numericality:{ only_integer: true,
+              greater_than_or_equal_to: 300,
+              less_than_or_equal_to: 9_999_999}
   end
-  validates :category_id, :sales_status_id, :shipping_fee_status_id, :prefecture_id, :scheduled_delivery_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :category_id, :sales_status_id,
+            :shipping_fee_status_id, :prefecture_id,
+            :scheduled_delivery_id,
+            numericality: { other_than: 1 , message: "can't be blank"}
 end
