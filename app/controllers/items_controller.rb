d031a6a904575d9f9d91ef485c098f19272a3c55
MatsunagaNaoya <60386488+MatsunagaNaoya@users.noreply.github.com>
2022-10-16 17:23:59 +0900

    ログアウト時に出品ページ遷移すると、ログインページへ遷移

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,6 @@
 class ItemsController < ApplicationController
+  before_action :move_to_index, except: [:index]
+
   def index
 #    @items = Item.includes(:user).order("created_at DESC")
   end
@@ -20,5 +22,11 @@ class ItemsController < ApplicationController
 
   def item_params
     params.require(:item).permit(:image, :name, :info, :category_id, :sales_status_id, :shipping_fee_status_id, :prefecture_id, :scheduled_delivery_id, :price).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to new_user_session_path
+    end
   end
 end
