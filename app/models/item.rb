1847e9604d72e2ea9d2f13632e8233f4a2b369d3
MatsunagaNaoya <60386488+MatsunagaNaoya@users.noreply.github.com>
2022-10-16 10:56:37 +0900

    SalesStatus id設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,6 +3,11 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
   belongs_to :category
+  belongs_to :sales_status
+  belongs_to :shipping_fee_status
+  belongs_to :prefecture
+  belongs_to :scheduled_delivery
+  
 
   validates :name, :info, :price, presence: true
   validates :category_id, :sales_status_id, :shipping_fee_status_id, :prefecture_id, :scheduled_delivery_id, numericality: { other_than: 1 , message: "can't be blank"}
