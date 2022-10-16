be46045c9bbfcb08a3a288db748238a59a6086f3
MatsunagaNaoya <60386488+MatsunagaNaoya@users.noreply.github.com>
2022-10-16 19:58:50 +0900

    指摘箇所修正・追加

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -13,6 +13,11 @@ RSpec.describe Item, type: :model do
       end
     end
     context '商品新規登録できないとき' do
+      it 'userが紐づいてないと出品できない' do
+        @item.user = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("User must exist")
+      end
       it 'imageが空では登録できない' do
         @item.image = nil
         @item.valid?
