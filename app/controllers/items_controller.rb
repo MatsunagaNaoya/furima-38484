533afb8952e77338a6e0d4aa4c61e3763b668c10
MatsunagaNaoya <60386488+MatsunagaNaoya@users.noreply.github.com>
2022-10-16 10:46:00 +0900

    Category id設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,18 +7,18 @@ class ItemsController < ApplicationController
     @item = Item.new
   end
 
-    def create
-      @item = Item.create(item_params)
-      if @item.save
-        redirect_to root_path
-      else
-        render :new
-      end
+  def create
+    @item = Item.create(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
     end
+  end
 
-    private
+  private
 
-    def item_params
-      params.require(:item).permit(:image, :name, :info, :category_id, :sales_status_id, :shipping_fee_status_id, :prefecture_id, :scheduled_delivery_id, :price).merge(user_id: current_user.id)
-    end
+  def item_params
+    params.require(:item).permit(:image, :name, :info, :category_id, :sales_status_id, :shipping_fee_status_id, :prefecture_id, :scheduled_delivery_id, :price).merge(user_id: current_user.id)
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,9 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :user
+  has_one_attached :image
+  belongs_to :category
+
+  validates :name, :info, :price, presence: true
+  validates :category_id, :sales_status_id, :shipping_fee_status_id, :prefecture_id, :scheduled_delivery_id, numericality: { other_than: 1 , message: "can't be blank"}
 end
