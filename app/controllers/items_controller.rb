d7337df9ecba09dcd8711b48b6351bbdbb922559
MatsunagaNaoya <60386488+MatsunagaNaoya@users.noreply.github.com>
2022-10-18 16:53:48 +0900

    ログアウト状態時の遷移を、authenticate_user!メソッドに変更

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :set_item, only: [:edit, :show, :update]
-  before_action :move_to_index, except: [:index, :show]
+  before_action :authenticate_user!, except: [:index, :show]
 
   def index
     @items = Item.includes(:user).order('created_at DESC')
@@ -45,7 +45,4 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
-  def move_to_index
-    redirect_to new_user_session_path unless user_signed_in?
-  end
 end
