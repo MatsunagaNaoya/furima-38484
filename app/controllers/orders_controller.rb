b19b8b08e26a9766eec0b4debe499d7aa8ffde42
MatsunagaNaoya <60386488+MatsunagaNaoya@users.noreply.github.com>
2022-10-21 18:55:15 +0900

    Order修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,12 +5,20 @@ class OrdersController < ApplicationController
   end
 
   def create
-    Order.create(order_params)
+    @item = Item.find(params[:item_id])
+    @order_pay_form = OrderPayForm.new(order_params)
+    if @order_pay_form.valid?
+      @order_pay_form.save
+      redirect_to root_path
+    else
+      render :index
+    end
   end
 
   private
   def order_params
-    params.require(:order_pay_form).permit(:postal_code, :prefecture_id, :city, :addresses, :building, :phone_number).merge(user_id: current_user.id, item_id: current_user.item.id)
+    params.require(:order_pay_form).permit(:postal_code, :prefecture_id, :city, :addresses, :building, :phone_number)
+    .merge(user_id: current_user.id, item_id: params[:item_id])
   end
 
 end
--- app/models/order_pay_form.rb
+++ app/models/order_pay_form.rb
@@ -3,8 +3,8 @@ class OrderPayForm
   attr_accessor :user_id, :item_id, :postal_code, :prefecture_id,:city, :addresses, :building, :phone_number
 
   with_options presence: true do
-    validates :user_id, :item_id,
-              :prefecture_id, :city, :addresses 
+    validates :user_id, :item_id, :city, :addresses
+    validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates :phone_number, format: {with: /\A\d{10,11}\z/}
   end
