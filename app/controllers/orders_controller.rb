5ee937985bf0e88c2eff5c8b43312d7b71e04b1c
MatsunagaNaoya <60386488+MatsunagaNaoya@users.noreply.github.com>
2022-10-21 14:33:27 +0900

    コントローラー作成

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,16 @@
 class OrdersController < ApplicationController
+  def index
+    @item = Item.find(params[:item_id])
+    @order_pay_form = OrderPayForm.new
+  end
+
+  def create
+    Order.create(order_params)
+  end
+
+  private
+  def order_params
+    params.require(:order_pay_form).permit(:postal_code, :prefecture_id, :city, :addresses, :building, :phone_number).merge(user_id: current_user.id, item_id: current_user.item.id)
+  end
+
 end
