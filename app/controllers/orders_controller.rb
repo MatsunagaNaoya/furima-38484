bc527073201a2b0da36ea10adf5118e21dab6afe
MatsunagaNaoya <60386488+MatsunagaNaoya@users.noreply.github.com>
2022-10-22 16:13:44 +0900

    対象外をrootパスへ遷移実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,7 @@
 class OrdersController < ApplicationController
+  before_action :move_to_index
+  before_action :authenticate_user!
+
   def index
     @item = Item.find(params[:item_id])
     @order_pay_form = OrderPayForm.new
@@ -32,4 +35,11 @@ class OrdersController < ApplicationController
     )
   end
 
+  def move_to_index
+    @item = Item.find(params[:item_id])
+    if (current_user == @item.user) || @item.order
+      redirect_to root_path
+    end
+  end
 end
+#/items/1/orders
