1c16debd3d0d766089eac0a61e919c6890e24abf
MatsunagaNaoya <60386488+MatsunagaNaoya@users.noreply.github.com>
2022-10-25 15:28:27 +0900

    OrderPayFormテストエラー文日本語対応

--- spec/models/order_pay_form_spec.rb
+++ spec/models/order_pay_form_spec.rb
@@ -22,62 +22,62 @@ RSpec.describe OrderPayForm, type: :model do
     it 'postal_codeが空では購入できない' do
       @order_pay_form.postal_code = ''
       @order_pay_form.valid?
-      expect(@order_pay_form.errors.full_messages).to include("Postal code can't be blank")
+      expect(@order_pay_form.errors.full_messages).to include("郵便番号を入力してください")
     end
     it 'postal_codeにハイフン(-)無しでは購入できない' do
       @order_pay_form.postal_code = '1234567'
       @order_pay_form.valid?
-      expect(@order_pay_form.errors.full_messages).to include('Postal code is invalid. Include hyphen(-)')
+      expect(@order_pay_form.errors.full_messages).to include('郵便番号ハイフン(-)を入力してください')
     end
     it 'Prefecture_idが1番(---)では登録できない' do
       @order_pay_form.prefecture_id = 1
       @order_pay_form.valid?
-      expect(@order_pay_form.errors.full_messages).to include("Prefecture can't be blank")
+      expect(@order_pay_form.errors.full_messages).to include("都道府県を選択してください")
     end
     it 'cityが空では登録できない' do
       @order_pay_form.city = ''
       @order_pay_form.valid?
-      expect(@order_pay_form.errors.full_messages).to include("City can't be blank")
+      expect(@order_pay_form.errors.full_messages).to include("市区町村を入力してください")
     end
     it 'addressesが空では登録できない' do
       @order_pay_form.addresses = ''
       @order_pay_form.valid?
-      expect(@order_pay_form.errors.full_messages).to include("Addresses can't be blank")
+      expect(@order_pay_form.errors.full_messages).to include("番地を入力してください")
     end
     it 'phone_numberが空では登録できない' do
       @order_pay_form.phone_number = ''
       @order_pay_form.valid?
-      expect(@order_pay_form.errors.full_messages).to include("Phone number can't be blank")
+      expect(@order_pay_form.errors.full_messages).to include("電話番号を入力してください")
     end
     it 'phone_numberが9桁以下では登録できない' do
       @order_pay_form.phone_number = '123456789'
       @order_pay_form.valid?
-      expect(@order_pay_form.errors.full_messages).to include('Phone number is invalid')
+      expect(@order_pay_form.errors.full_messages).to include('電話番号は不正な値です')
     end
     it 'phone_numberが12桁以上では登録できない' do
       @order_pay_form.phone_number = '000044448888'
       @order_pay_form.valid?
-      expect(@order_pay_form.errors.full_messages).to include('Phone number is invalid')
+      expect(@order_pay_form.errors.full_messages).to include('電話番号は不正な値です')
     end
     it 'phone_numberが英数混合では登録できない' do
       @order_pay_form.phone_number = '012o-21090'
       @order_pay_form.valid?
-      expect(@order_pay_form.errors.full_messages).to include('Phone number is invalid')
+      expect(@order_pay_form.errors.full_messages).to include('電話番号は不正な値です')
     end
     it 'tokenが空では登録できないこと' do
       @order_pay_form.token = nil
       @order_pay_form.valid?
-      expect(@order_pay_form.errors.full_messages).to include("Token can't be blank")
+      expect(@order_pay_form.errors.full_messages).to include("クレジットカード情報を入力してください")
     end
     it 'user_idが無いと購入できない' do
       @order_pay_form.user_id = ''
       @order_pay_form.valid?
-      expect(@order_pay_form.errors.full_messages).to include("User can't be blank")
+      expect(@order_pay_form.errors.full_messages).to include("Userを入力してください")
     end
     it 'item_idが無いと購入できない' do
       @order_pay_form.item_id = ''
       @order_pay_form.valid?
-      expect(@order_pay_form.errors.full_messages).to include("Item can't be blank")
+      expect(@order_pay_form.errors.full_messages).to include("Itemを入力してください")
     end
   end
 end
